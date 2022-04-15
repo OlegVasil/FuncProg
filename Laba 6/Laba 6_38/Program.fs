14b089e49ecdb03c4f2d99b75386584d95e9a469
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2022-04-15 18:13:52 +0300

    EasyPeasy

---
+++ Laba 6/Laba 6_38/Program.fs
@@ -0,0 +1,30 @@
+﻿open System 
+
+let rec ReadList n = 
+    if n=0 then []
+    else 
+    let Head = Convert.ToInt32(Console.ReadLine()) 
+    let Tail = ReadList (n-1)
+    Head::Tail
+
+let Amount list a b = 
+    let rec amount list a b count =
+        match list with 
+        | h::t->
+            if (h>=a && h<=b) then amount t a b (count+1)
+            else amount t a b count 
+        | []->count 
+    amount list a b 0
+
+[<EntryPoint>]
+let main argv = 
+    Console.WriteLine("Введите интервал: ")
+    Console.Write("a: ")
+    let a = Convert.ToInt32(Console.ReadLine())
+    Console.Write("b: ")
+    let b = Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    let n = Convert.ToInt32(Console.ReadLine())
+    let list = ReadList n
+    Console.WriteLine(Amount list a b )
+    0
