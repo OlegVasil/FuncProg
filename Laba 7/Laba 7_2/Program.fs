69efddfd2b0bdb4913eb2736276f2b8a9986fbd5
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2022-04-26 12:22:35 +0300

    New

---
+++ Laba 7/Laba 7_2/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+let rec ReadList n =
+    List.init(n) (fun index->Convert.ToInt32(Console.ReadLine()))
+let rec WriteList list=
+    List.iter(fun x->printfn "%O" x) list
+let Min_id list=List.findIndex(fun x->x=List.min list) list
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите список: ")
+    let n = Convert.ToInt32(Console.ReadLine())
+    let list= ReadList n
+    Console.WriteLine(Min_id list+1)
+    0
