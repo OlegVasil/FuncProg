d9ede10a812a2f14fa7f5bd4df5fc64a51b48c0e
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2022-04-30 10:18:54 +0300

    number 32

---
+++ Laba 7/Laba 7_32/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+ 
+let rec ReadList n =
+    List.init(n) (fun index->Convert.ToInt32(Console.ReadLine()))
+
+let rec WriteList list=
+    List.iter(fun x->printfn "%O" x) list
+
+let Result_List (list:'int list)=List.fold (fun s x->if x then s+1 else s) 0 (List.map3 (fun x y z->(y>x)&&(y>z)) list.[0..list.Length-3] list.[1..list.Length-2] list.[2..list.Length-1]) 
+
+[<EntryPoint>]
+let main argv =
+
+    Console.WriteLine("Введите список: ")
+    let n = Convert.ToInt32(Console.ReadLine())
+    let list= ReadList n
+    Console.WriteLine("Результат: ")
+    Console.WriteLine(Result_List list)
+    0
