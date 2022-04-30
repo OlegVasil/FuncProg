7c05762315c72dc0743eeaa883600313bd26437d
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2022-04-30 10:35:05 +0300

    42 added

---
+++ Laba 7/Laba 7_42/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+ //локальный максимум - элемент, больше двух "соседних".
+let rec ReadList n =
+    List.init(n) (fun index->Convert.ToDouble(Console.ReadLine()))
+
+let rec WriteList list=
+    List.iter(fun x->printfn "%O" x) list
+
+let Result_List list= List.filter (fun x->x<List.average list) list
+
+[<EntryPoint>]
+let main argv =
+
+    Console.WriteLine("Введите список: ")
+    let n = Convert.ToInt32(Console.ReadLine())
+    let list= ReadList n
+    Console.WriteLine("Результат: ")
+    Console.WriteLine(Result_List list)
+    0
