9759ebed379798688f9591d1789eb04d44a94982
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2022-04-30 23:06:51 +0300

    Addddd 52

---
+++ Laba 7/Laba 7_52/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+let rec ReadList n =
+    List.init(n) (fun index->Convert.ToDouble(Console.ReadLine()))
+
+let rec WriteList list=
+    List.iter(fun x->printfn "%O" x) list
+
+let Simple number =
+    let rec simp num del =
+        if num <= 1 then []
+        else 
+            let new_del = del+1
+            if num % del =0 then List.append[del] (simp (num/del) del)                        
+            else simp num new_del  
+    simp number 2 
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите ваше число: ")
+    let number = System.Convert.ToInt32(System.Console.ReadLine())
+    WriteList(Simple number)
+    0
