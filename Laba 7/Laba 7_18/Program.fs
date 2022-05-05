f1b0af2ebe0ed5d8901a9ae0cbd72bab507af621
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2022-05-05 22:18:39 +0300

    Update Program.fs

--- Laba 7/Laba 7_18/Program.fs
+++ Laba 7/Laba 7_18/Program.fs
@@ -1,2 +1,15 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open System
+
+let rec ReadList n =
+    List.init(n) (fun index->Convert.ToInt32(Console.ReadLine()))
+
+let rec WriteList list=
+    List.iter(fun x->printfn "%O" x) list
+
+[<EntryPoint>]
+let main argv =
+    let array1=[|1;2;3|]
+    let array2=[|4;5;7|]
+    let result=Array.append array1 [|(Array.item(array2.Length-1) array2)|] 
+    printfn"%A"result
+    0 
