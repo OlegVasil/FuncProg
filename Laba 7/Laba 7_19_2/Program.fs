3e16dd4715399bd28d5aad974cf91e62b9b85ca1
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2022-05-05 22:28:48 +0300

    Add files via upload

---
+++ Laba 7/Laba 7_19_2/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+let rec ReadList n =
+    List.init(n) (fun index->Convert.ToInt32(Console.ReadLine()))
+
+let rec WriteList list=
+    List.iter(fun x->printfn "%O" x) list
+
+let Order str=
+    let lower_str=String.filter(fun ch->Char.IsLower(ch)) str
+    let srt_list=Seq.toList(lower_str)
+    let result=List.map2(fun ch1 ch2-> ch1<=ch2) srt_list.[0..srt_list.Length-2] srt_list.[1..srt_list.Length-1] 
+    if (List.fold (fun s x-> if x=false then s+1 else s) 0 result) = 0 then Console.WriteLine("Упорядочены")
+    else Console.WriteLine("Не упорядочены")
+
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите строку")
+    let str = Console.ReadLine()
+    Order str
+    0 
