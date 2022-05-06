43d23f30c511f7d6642715e369beaca7f2f4b652
OlegVasil <100850508+OlegVasil@users.noreply.github.com>
2022-05-06 22:22:30 +0300

    Update

---
+++ Laba 7/Laba 7_19_10/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let rec ReadList n =
+    List.init(n) (fun index->Convert.ToInt32(Console.ReadLine()))
+
+let rec WriteList list=
+    List.iter(fun x->printfn "%O" x) list
+
+let Counta str =
+    let new_str=String.filter(fun ch->ch='A') str
+    Console.WriteLine(new_str.Length)
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите строку")
+    let str = Console.ReadLine()
+    Counta str
+    0 
